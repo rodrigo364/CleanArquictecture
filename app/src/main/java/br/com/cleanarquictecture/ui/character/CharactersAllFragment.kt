adc7ddc6e416514489914fef76ef5d28bc3b1f03
Rodrigo Fonseca de Liz <digaorfl@hotmail.com>
2023-08-03 11:21:26 -0300

    feat: implementation UiState flow

--- app/src/main/java/br/com/cleanarquictecture/ui/character/CharactersAllFragment.kt
+++ app/src/main/java/br/com/cleanarquictecture/ui/character/CharactersAllFragment.kt
@@ -1,5 +1,6 @@
 package br.com.cleanarquictecture.ui.character
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,9 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import br.com.cleanarquictecture.R
+import br.com.cleanarquictecture.ui.UiState
 import br.com.core.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CharactersAllFragment : Fragment() {
@@ -25,27 +31,32 @@ class CharactersAllFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_characters_all, container, false)
     }
 
+    @SuppressLint("UnsafeRepeatOnLifecycleDetector")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getCharactersAll()
-        viewModel.characterData.observe(viewLifecycleOwner) { result ->
-            when (result) {
-                is Resource.Loading -> {
-                Log.d("RESOURCE","LOADING..")
-                }
 
-                is Resource.Success -> {
-                    Log.d("RESOURCE","SUCCESS.."+ result.data!!.info.count)
-
-                }
-
-                is Resource.Failure -> {
-                    Log.d("RESOURCE","FAILURE.."+ result.message)
-
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect{
+                    when(it) {
+                        is UiState.Loading -> {
+                            Log.d("SUC"," it.data?.info?.next!!")
+                        }
+                        is UiState.Success -> {
+                            Log.d("SUC", it.data?.info?.next!!)
+                        }
+                        is  UiState.Error -> {
+                            Log.d("TESTE", it.message!!)
+                        }
+                    }
                 }
             }
-
         }
+
+
+
+
     }
 
 }
--- app/src/main/java/br/com/cleanarquictecture/ui/character/CharactersViewModel.kt
+++ app/src/main/java/br/com/cleanarquictecture/ui/character/CharactersViewModel.kt
@@ -4,10 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.cleanarquictecture.ui.UiState
 import br.com.core.Resource
 import br.com.domain.model.CharacterAll
 import br.com.domain.usecase.CharacterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,14 +20,20 @@ class CharactersViewModel @Inject constructor(
     private val charactersUseCase : CharacterUseCase
 ): ViewModel() {
 
+    private val _uiState = MutableStateFlow<UiState<CharacterAll>>(UiState.Loading)
+    val uiState: StateFlow<UiState<CharacterAll>> = _uiState
     private val _characterData = MutableLiveData<Resource<CharacterAll>>()
     val characterData: LiveData<Resource<CharacterAll>> = _characterData
 
     fun getCharactersAll() {
 
         viewModelScope.launch {
-            charactersUseCase().collect { resource ->
-                _characterData.value = resource
+            charactersUseCase()
+                .catch {
+                    _uiState.value = UiState.Error(it.message)
+                }
+                .collect { resource ->
+                _uiState.value = UiState.Success(resource.data)
             }
         }
     }
